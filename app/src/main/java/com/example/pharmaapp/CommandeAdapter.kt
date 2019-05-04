97bd5b162b07890b3445bb0f6d595bbd7c25fb30
Nadji <fm_souilamas@esi.dz>
2019-05-04 12:34:42 +0100

    in case commit

--- app/src/main/java/com/example/pharmaapp/CommandeAdapter.kt
+++ app/src/main/java/com/example/pharmaapp/CommandeAdapter.kt
@@ -2,22 +2,44 @@ package com.example.pharmaapp
 
 import android.content.Context
 import android.graphics.Color
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 
-class CommandeAdapter(val ctx: Context, val items: List<Commande>): BaseAdapter() {
+class CommandeAdapter(val ctx: Context, val items: List<Commande>): RecyclerView.Adapter<CommandeAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+val inflatedView = parent.inflate(R.layout.command_item)
+        return CommandeHolder(inflatedView)
+    }
 
-    private class ViewHolder(val nom_date: TextView, val pharmacie : TextView, val etat: TextView)
+    override fun getItemCount(): Int {
+      return items.size
+    }
+
+    override fun onBindViewHolder(holder: CommandeAdapter.CommandeHolder, position: Int) {
+        val itemCommande = items[position]
+        holder.bindCommande(itemCommande)
+    }
+
+     class CommandeHolder(v : View) : RecyclerView.ViewHolder(v), View.OnClickListener{
+        override fun onClick(v: View?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        fun bindCommande(itemCommande: Commande) {
+
+        }
+    }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var view = convertView
         val viewHolder: ViewHolder
         if (view == null) {
 
-            view = LayoutInflater.from(ctx).inflate(R.layout.command_layout,parent,false)
+            view = LayoutInflater.from(ctx).inflate(R.layout.command_item,parent,false)
             val nom = view?.findViewById(R.id.nom_date) as TextView
             val pharmacie = view?.findViewById(R.id.pharmacie) as TextView
             val etat = view?.findViewById(R.id.etat) as TextView
